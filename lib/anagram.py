932b36b3b88416e4ca68bf5caba3cf9aa8154afb
kamaukimani <paulkimys285@gmail.com>
2025-08-13 01:58:52 +0300

    update

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+        self.word_list = []  
+
+    def match(self, word_list):
+        self.word_list = word_list
+        matches = []
+        for candidate in self.word_list:
+            if candidate.lower() == self.word:
+                continue
+            if sorted(candidate.lower()) == self.sorted_word:
+                matches.append(candidate)
+        return matches
